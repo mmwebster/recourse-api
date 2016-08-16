27681c43063afd5f47ea0ee84951f73b71e897f1
mmwebster <milo.webster@gmail.com>
2016-08-16 16:52:08 -0700

    cleaned up controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,3 @@
 class Api::V1::UsersController < ApiController
   skip_before_action :authenticate_user!, only: [:create]
-  after_filter do
-    Rails.logger.debug "UsersController: ".green + response.body
-  end
 end
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,13 @@
 class ApiController < ApplicationController
   include JSONAPI::ActsAsResourceController
 
+  # after_filter do
+  #   Rails.logger.debug("RESPONSE: ".green + response.body)
+  # end
+  #
   after_action :before_api
 
   def before_api
-    Rails.logger.debug("STATUS:".green + " API endpoint triggered.")
+    Rails.logger.debug("STATUS: ".green + " API endpoint triggered.")
   end
 end
